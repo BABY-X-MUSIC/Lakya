90303a0d3e2dbf87595d8170cf7e8623c6aac171
Sтσям <118429184+StormBeatz@users.noreply.github.com>
2025-04-07 12:44:34 +0530

    Update play.py

--- Lyka/utils/inline/play.py
+++ Lyka/utils/inline/play.py
@@ -75,13 +75,6 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
             )
         ],
       [
-            InlineKeyboardButton(text="❍Owner ❍", url="https://t.me/GrayBotSupport",),
-            InlineKeyboardButton(text="❍Support❍", url="https://t.me/GrayBotSupport",),
-        ],
-        [
-            InlineKeyboardButton(text=" Add Me Baby", url="https://t.me/LykaRobot?startgroup=true",),
-        ],
-        [
             InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")
         ],
     ]
@@ -98,13 +91,6 @@ def stream_markup(_, videoid, chat_id):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
          ],
-        [
-            InlineKeyboardButton(text="❍Owner ❍", url="https://t.me/GrayBotSupport",),
-            InlineKeyboardButton(text="❍Support❍", url="https://t.me/GrayBotSupport",),
-        ],
-        [
-            InlineKeyboardButton(text=" Add Me Baby", url="https://t.me/LykaRobot?startgroup=true",),
-        ],
         [
             InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")
         ],
