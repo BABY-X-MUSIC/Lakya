523bd8f8cddd0b2d699951e86a34f98349ba28da
Sтσям <118429184+StormBeatz@users.noreply.github.com>
2025-04-13 06:30:00 +0530

    Update thumbnails.py

--- Lyka/utils/thumbnails.py
+++ Lyka/utils/thumbnails.py
@@ -120,14 +120,3 @@ async def get_thumb(videoid):
         print(e)
         return YOUTUBE_IMG_URL
         
-        
-        
-        async def get_qthumb(vidid):
-    try:
-        query = f"https://www.youtube.com/watch?v={vidid}"
-        results = VideosSearch(query, limit=1)
-        for result in (await results.next())["result"]:
-            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
-        return thumbnail
-    except Exception as e:
-        return YOUTUBE_IMG_URLE_IMG_URL
